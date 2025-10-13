21b15cb8a36b4c743676a7fc775568491b2c33bb
Nicoomega <santiago1025arba@gmail.com>
2025-10-13 18:56:45 -0500

    listo para el arranque

---
+++ db/bootstrap.sql
@@ -0,0 +1,8 @@
+﻿CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS citext;
+
+CREATE TABLE IF NOT EXISTS category (
+  id   uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+  slug citext UNIQUE NOT NULL,
+  name text NOT NULL
+);
